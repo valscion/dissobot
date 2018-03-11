84176f518b63b022f631beb559c1b55acd08c5d7
Vesa Laakso <laakso.vesa@gmail.com>
2018-03-11 18:36:14 +0200

    Config app to use local DynamoDB when offline

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const express = require('express')
 const app = express()
 const AWS = require('aws-sdk');
 
-
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
 
 app.use(bodyParser.json({ strict: false }));
 
