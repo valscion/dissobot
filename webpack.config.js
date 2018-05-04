326ef59fd104e5b6134c0e1e8e6bbc35232600a2
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-04 21:18:50 +0300

    Configure webpack+babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
-const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: slsw.lib.entries,
-  target: 'node',
-  // We might want to consider enabling production mode if the build gets overly large.
-  // Right now in the beginning, the build is so small that it can be easily analyzed on its own.
-  mode: 'development',
-  // Keep `require('some-node-module')` as-is in the bundle
+  target: "node",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false
+  },
+  devtool: false,
   externals: [nodeExternals()],
-  // Sourcemaps are not needed, it's easier to debug the build output this way
-  devtool: false
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    libraryTarget: "commonjs2",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js"
+  }
 };
