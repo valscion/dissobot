accf59caac325c60c6c2de83bba99a5cbf765b19
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-04 20:00:53 +0300

    Move fetch to correct module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const express = require("express");
-const fetch = require("node-fetch");
 const app = express();
 const AWS = require("aws-sdk");
 
--- telegram.js
+++ telegram.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 module.exports = {
   sendMessage
 };
