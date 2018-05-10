a7dbcf450bd8173c6e823cc02ed28f24cd35d252
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-10 22:01:50 +0300

    Make gsheet async

--- src/gsheet/handler.js
+++ src/gsheet/handler.js
@@ -2,7 +2,7 @@
 
 import type { APIGatewayEvent, ProxyResult } from "../common/types";
 
-export default function updateSpreadsheetHandler(
+export default async function updateSpreadsheetHandler(
   event: APIGatewayEvent,
   _context: empty,
   callback: (error: null | Error, result?: ProxyResult) => void
