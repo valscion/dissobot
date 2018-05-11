eaae76b8435e39742b1e4daa04be982d6a821f19
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-11 16:55:23 +0300

    Log worksheets info when gsheet handler is called

--- src/gsheet/handler.js
+++ src/gsheet/handler.js
@@ -2,12 +2,18 @@
 
 import type { APIGatewayEvent, ProxyResult } from "../common/types";
 
+import { getInfo } from "./spreadsheet";
+
 export default async function updateSpreadsheetHandler(
   event: APIGatewayEvent,
   _context: empty,
   callback: (error: null | Error, result?: ProxyResult) => void
 ) {
-  console.log("Called updateSpreadsheetHandler", JSON.stringify(event));
+  const info = await getInfo();
+  console.log({
+    title: info.title,
+    sheets: info.worksheets.map(sh => sh.title).join(", ")
+  });
 
   callback(null, {
     statusCode: 200,
