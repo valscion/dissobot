0f7223d0dd0d1190c6c89e87a4df716fce6ceafe
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-20 21:04:27 +0300

    Remove unused DynamoDB from telegram index

--- telegram.js
+++ telegram.js
@@ -1,22 +1,6 @@
 // @flow
 
 import "babel-polyfill";
-import AWS from "aws-sdk";
-
-import { IS_OFFLINE } from "./src/common/environment";
 import handler from "./src/telegram/handler";
-
-let dynamoDb;
-if (IS_OFFLINE === "true") {
-  dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: "localhost",
-    endpoint: "http://localhost:8000",
-    accessKeyId: "indifferent",
-    secretAccessKey: "lul im a sekrit"
-  });
-  // console.log(dynamoDb);
-} else {
-  dynamoDb = new AWS.DynamoDB.DocumentClient();
-}
 
 module.exports.handler = handler;
