758283f3fd7a640fd167dc1483098d3d55bb6869
Vesa Laakso <laakso.vesa@gmail.com>
2018-05-20 20:47:54 +0300

    Switch from users to ilmos table

--- src/common/environment.js
+++ src/common/environment.js
@@ -5,12 +5,12 @@ const env = process.env;
 if (!env.TELEGRAM_TOKEN) throw new Error("TELEGRAM_TOKEN env var is missing");
 if (!env.TELEGRAM_URL_SECRET)
   throw new Error("TELEGRAM_URL_SECRET env var is missing");
-if (!env.USERS_TABLE) throw new Error("USERS_TABLE env var is missing");
+if (!env.ILMOS_TABLE) throw new Error("ILMOS_TABLE env var is missing");
 
 module.exports = {
   TELEGRAM_TOKEN: env.TELEGRAM_TOKEN,
   TELEGRAM_URL_SECRET: env.TELEGRAM_URL_SECRET,
-  USERS_TABLE: env.USERS_TABLE,
+  ILMOS_TABLE: env.ILMOS_TABLE,
   SPREADSHEET_SERVICE_ACCOUNT_CLIENT_EMAIL:
     env.SPREADSHEET_SERVICE_ACCOUNT_CLIENT_EMAIL,
   SPREADSHEET_SERVICE_ACCOUNT_PRIVATE_KEY:
