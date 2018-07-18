b6795ae1625da979e96a58cb71c965a7f41fc94d
Vesa Laakso <laakso.vesa@gmail.com>
2018-07-18 10:34:20 +0300

    Remove parts of eslint config we don't use

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
-var restrictedGlobals = require("confusing-browser-globals");
-
 module.exports = {
   root: true,
 
   parser: "babel-eslint",
 
-  plugins: ["import", "flowtype", "jsx-a11y", "react"],
+  plugins: ["flowtype"],
 
   env: {
-    browser: true,
     commonjs: true,
     es6: true,
     jest: true,
@@ -93,7 +90,7 @@ module.exports = {
     "no-this-before-super": "warn",
     "no-throw-literal": "warn",
     "no-undef": "error",
-    "no-restricted-globals": ["error"].concat(restrictedGlobals),
+    "no-restricted-globals": ["error"],
     "no-unexpected-multiline": "warn",
     "no-unreachable": "warn",
     "no-unused-expressions": [
@@ -140,75 +137,7 @@ module.exports = {
     "unicode-bom": ["warn", "never"],
     "use-isnan": "warn",
     "valid-typeof": "warn",
-    "no-restricted-properties": [
-      "error",
-      {
-        object: "require",
-        property: "ensure",
-        message:
-          "Please use import() instead. More info: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#code-splitting"
-      },
-      {
-        object: "System",
-        property: "import",
-        message:
-          "Please use import() instead. More info: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#code-splitting"
-      }
-    ],
     "getter-return": "warn",
-
-    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
-    "import/first": "error",
-    "import/no-amd": "error",
-    "import/no-webpack-loader-syntax": "error",
-
-    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
-    "react/forbid-foreign-prop-types": ["warn", { allowInPropTypes: true }],
-    "react/jsx-no-comment-textnodes": "warn",
-    "react/jsx-no-duplicate-props": ["warn", { ignoreCase: true }],
-    "react/jsx-no-target-blank": "warn",
-    "react/jsx-no-undef": "error",
-    "react/jsx-pascal-case": [
-      "warn",
-      {
-        allowAllCaps: true,
-        ignore: []
-      }
-    ],
-    "react/jsx-uses-react": "warn",
-    "react/jsx-uses-vars": "warn",
-    "react/no-danger-with-children": "warn",
-    "react/no-deprecated": "warn",
-    "react/no-direct-mutation-state": "warn",
-    "react/no-is-mounted": "warn",
-    "react/react-in-jsx-scope": "error",
-    "react/require-render-return": "error",
-    "react/style-prop-object": "warn",
-
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-    "jsx-a11y/accessible-emoji": "warn",
-    "jsx-a11y/alt-text": "warn",
-    "jsx-a11y/anchor-has-content": "warn",
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
-      {
-        aspects: ["noHref", "invalidHref"]
-      }
-    ],
-    "jsx-a11y/aria-activedescendant-has-tabindex": "warn",
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-role": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/heading-has-content": "warn",
-    "jsx-a11y/iframe-has-title": "warn",
-    "jsx-a11y/img-redundant-alt": "warn",
-    "jsx-a11y/no-access-key": "warn",
-    "jsx-a11y/no-distracting-elements": "warn",
-    "jsx-a11y/no-redundant-roles": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "jsx-a11y/scope": "warn",
 
     // https://github.com/gajus/eslint-plugin-flowtype
     "flowtype/define-flow-type": "warn",
