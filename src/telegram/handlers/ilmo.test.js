610c3e6688f14950ec4496d89531614a6f50c025
Vesa Laakso <laakso.vesa@gmail.com>
2018-09-11 10:04:30 +0300

    Add test for same-day ilmo output

--- src/telegram/handlers/ilmo.test.js
+++ src/telegram/handlers/ilmo.test.js
@@ -11,6 +11,10 @@ const ilmonneet = require("./ilmo").ilmonneet;
 jest.mock("../api");
 jest.mock("../../common/db");
 
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 describe("ilmonneet", () => {
   test("ilmo sorting", async () => {
     advanceTo(new Date(2018, 8, 2, 0, 0, 0)); // 2018-09-02
@@ -54,6 +58,52 @@ describe("ilmonneet", () => {
       expect.objectContaining({
         chat_id: 123,
         text: expect.stringContaining("ti 4.9.")
+      })
+    );
+  });
+
+  test("ilmo date cutoff", async () => {
+    advanceTo(new Date(2018, 8, 11, 12, 0, 0)); // 2018-09-11T12:00:00
+
+    const ilmos: Array<SingleIlmoObject> = [
+      {
+        dateAsWritten: "ma 10.9.",
+        songs: null,
+        attendingList: [],
+        notAttendingList: [],
+        unknownList: []
+      },
+      {
+        dateAsWritten: "ti 11.9.",
+        songs: null,
+        attendingList: [],
+        notAttendingList: [],
+        unknownList: []
+      },
+      {
+        dateAsWritten: "ti 18.9.",
+        songs: null,
+        attendingList: [],
+        notAttendingList: [],
+        unknownList: []
+      }
+    ];
+    // $FlowFixMe
+    require("../../common/db").scan.mockImplementationOnce(() => ({
+      Items: ilmos
+    }));
+
+    const sendMessage = require("../api").sendMessage;
+    const chat = {
+      id: 123,
+      type: "group"
+    };
+    await ilmonneet[1](chat);
+    expect(sendMessage).toHaveBeenCalledTimes(1);
+    expect(sendMessage).toHaveBeenCalledWith(
+      expect.objectContaining({
+        chat_id: 123,
+        text: expect.stringContaining("ti 11.9.")
       })
     );
   });
