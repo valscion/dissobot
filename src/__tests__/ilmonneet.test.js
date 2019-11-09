416ecc1f06fedd98cf93bb015fcc1b7779e70718
Vesa Laakso <laakso.vesa@gmail.com>
2019-11-09 15:02:10 +0200

    Add more test coverage for formatting

--- src/__tests__/ilmonneet.test.js
+++ src/__tests__/ilmonneet.test.js
@@ -10,6 +10,12 @@ import type { APIGatewayEvent, ProxyResult } from "../common/types";
 
 import * as tgApi from "../telegram/api";
 
+const tgSendMessage = jest
+  .spyOn(tgApi, "sendMessage")
+  .mockImplementation(() => Promise.resolve());
+
+beforeEach(() => tgSendMessage.mockClear());
+
 test("gsheet POST -> /ilmonneet", async () => {
   advanceTo(new Date(2018, 8, 2, 0, 0, 0)); // 2018-09-02
 
@@ -55,6 +61,72 @@ test("gsheet POST -> /ilmonneet", async () => {
   `);
 });
 
+describe("gsheet POST -> /show <date>", () => {
+  beforeEach(async () => {
+    advanceTo(new Date(2018, 8, 2, 0, 0, 0)); // 2018-09-02
+    const sheetData = md`
+      |               |        |             | Soprano | Alto | Alto | Tenor 2 | Tenor 1 | Bass 1 | Bass 2 |
+      | ------------- | ------ | ----------- | ------- | ---- | ---- | ------- | ------- | ------ | ------ |
+      | Pvm           | Biisit | Tulossa (x) | SopA    | AltA | AltB | TenA    | TenB    | BasA   | BasB   |
+      | 5.11. all     |        |             | x       | x    | x    | x       | x       | x      | x      |
+      | 6.11. unknown |        |             |         |      |      |         |         |        |        |
+      | 7.11. none    |        |             | -       | -    | -    | -       | -       | -      | -      |
+    `;
+    await lambdaCall(gsheetHandler, { body: sheetData });
+  });
+
+  test("all attending", async () => {
+    expect(await replyTextFromShowCommand("5.11. all")).toMatchInlineSnapshot(`
+      "<b>5.11. all</b>
+
+      Songs not yet input
+
+      <i>7 coming, 0 not coming, 0 have not answered yet.</i>
+      <i>Attendees are:</i>
+      - SopA
+      - AltA
+      - AltB
+      - TenA
+      - TenB
+      - BasA
+      - BasB"
+    `);
+  });
+
+  test("unknown attendance for all", async () => {
+    expect(await replyTextFromShowCommand("6.11. unknown"))
+      .toMatchInlineSnapshot(`
+      "<b>6.11. unknown</b>
+
+      Songs not yet input
+
+      <i>0 coming, 0 not coming, 7 have not answered yet.</i>
+      <i>Attendees are:</i>
+      - No attendees yet"
+    `);
+  });
+
+  test("none attending", async () => {
+    expect(await replyTextFromShowCommand("7.11. none")).toMatchInlineSnapshot(`
+      "<b>7.11. none</b>
+
+      Songs not yet input
+
+      <i>0 coming, 7 not coming, 0 have not answered yet.</i>
+      <i>Attendees are:</i>
+      - No attendees yet"
+    `);
+  });
+
+  test("unknown date", async () => {
+    expect(
+      await replyTextFromShowCommand("4.11. bad date")
+    ).toMatchInlineSnapshot(
+      `"I could not find an upcoming rehearsal \\"4.11. bad date\\" :("`
+    );
+  });
+});
+
 function ilmonneetCommand(): Update {
   return {
     update_id: 1,
@@ -66,6 +138,30 @@ function ilmonneetCommand(): Update {
         type: "group"
       },
       text: "/ilmonneet"
+    }
+  };
+}
+
+async function replyTextFromShowCommand(rowText: string) {
+  await lambdaCall(telegramHandler, {
+    body: showCommand(rowText),
+    path: "/telegram/TELEGRAM_URL_SECRET"
+  });
+  expect(tgSendMessage).toHaveBeenCalledTimes(1);
+  return tgSendMessage.mock.calls[0][0].text;
+}
+
+function showCommand(rowText: string): Update {
+  return {
+    update_id: 1,
+    message: {
+      message_id: 100,
+      date: Date.now(),
+      chat: {
+        id: 200,
+        type: "group"
+      },
+      text: `/show ${rowText}`
     }
   };
 }
